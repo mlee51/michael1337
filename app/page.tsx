d3e4dfde7704eaf7d15986ea0c6a4cb130685fd7
mlee51 <mleeteak13@gmail.com>
2024-07-19 13:10:00 -0400

    fixed build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe width="720" height="1280" src="https://www.youtube.com/embed/9sCYDZ877Pc?si=1Wz06ttPzRl44yok&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe
+        width="720"
+        height="1280"
+        src="https://www.youtube.com/embed/9sCYDZ877Pc?si=1Wz06ttPzRl44yok&amp;controls=0"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </main>
   );
 }
