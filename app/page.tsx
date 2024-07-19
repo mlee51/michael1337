5d2aa5c91bfc4c2c21f065b2c1542ec197dce957
mlee51 <mleeteak13@gmail.com>
2024-07-19 13:29:41 -0400

    fixed css

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe
-        width="720"
-        height="1280"
-        src="https://www.youtube.com/embed/9sCYDZ877Pc?si=1Wz06ttPzRl44yok&amp;controls=0"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+    <main className="w-full h-screen flex justify-center items-center">
+      <div className="relative w-[315px] h-[560px]">
+        <iframe
+          width="315"
+          height="560"
+          src="https://www.youtube.com/embed/9sCYDZ877Pc?si=1Wz06ttPzRl44yok"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
     </main>
   );
 }
